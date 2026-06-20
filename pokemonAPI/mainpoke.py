a2bcf4c2f627236e9ca22ac4318617a9cf11dc38
Pedro <ricardoepedroclash@gmail.com>
2026-06-20 15:26:55 -0300

    feat: add Pokemon API search

--- pokemonAPI/mainpoke.py
+++ pokemonAPI/mainpoke.py
@@ -3,13 +3,6 @@ import time
 
 base_url = "https://pokeapi.co/api/v2/"
 
-name = input('Digite o nome do pokemon que deseja consultar: ')
-print('Procurando...')
-time.sleep(6)
-print('Aguarde só mais um pouco')
-time.sleep(5)
-print('Finalizando as pesquisas')
-
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
@@ -19,12 +12,27 @@ def get_pokemon_info(name):
         return pokemon_data
     else:
         print(f"Failed to retrieve data {response.status_code}")
+        return None
 
-pokemon_name = name
-pokemon_info = get_pokemon_info(pokemon_name)
+name = input('Digite o nome do pokemon que deseja consultar: ').lower().strip()
+
+print('Procurando...')
+time.sleep(6)
+
+print('Aguarde só mais um pouco')
+time.sleep(5)
+
+pokemon_info = get_pokemon_info(name)
 
 if pokemon_info:
+    print('Finalizando pesquisas')
+    time.sleep(1)
+    
     print(f"Nome: {pokemon_info['name'].capitalize()}")
     print(f"Id: {pokemon_info['id']}")
     print(f"Altura: {pokemon_info['height']}")
     print(f"Peso: {pokemon_info['weight']}")
+    
+else:
+    print('Pokémon não encontrado')
+    
