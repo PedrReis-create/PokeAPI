21f755d98426b7af3c291fc0bd5d821ac22a47b8
Pedro <ricardoepedroclash@gmail.com>
2026-06-20 15:30:56 -0300

    fix: handle API connection errors

--- pokemonAPI/mainpoke.py
+++ pokemonAPI/mainpoke.py
@@ -5,11 +5,18 @@ base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
-    response = requests.get(url)
+    
+    try:
+        response = requests.get(url)
+    
+    except requests.exceptions.RequestException:
+        print('Erro: Não foi possível conectar na API')
+        return None
     
     if response.status_code == 200:
         pokemon_data = response.json()
         return pokemon_data
+    
     else:
         print(f"Failed to retrieve data {response.status_code}")
         return None
