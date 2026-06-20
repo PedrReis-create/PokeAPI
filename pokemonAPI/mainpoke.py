9b05cd4567093a144a2e2d0b82172e947c452f74
Pedro <ricardoepedroclash@gmail.com>
2026-06-20 16:05:32 -0300

    style: improve terminal output formatting

--- pokemonAPI/mainpoke.py
+++ pokemonAPI/mainpoke.py
@@ -1,7 +1,33 @@
 import requests
 import time
+import sys
 
 base_url = "https://pokeapi.co/api/v2/"
+
+
+def menu():
+    
+    while True:
+        
+        print("===== POKEDEX =====")
+        print("1 - Buscar Pokémon")
+        print("2 - Ver histórico")
+        print("3 - Sair")
+        
+        try:
+            escolha = int(input("Escolha:"))
+        except ValueError:
+            print('Digite um valor válido')
+            continue
+        
+        if escolha == 1:
+            buscar_pokemon()
+        elif escolha == 2:
+            historico()
+        elif escolha == 3:
+            sys.exit()
+        
+    
 
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
@@ -21,34 +47,42 @@ def get_pokemon_info(name):
         print(f"Failed to retrieve data {response.status_code}")
         return None
 
-name = input('Digite o nome do pokemon que deseja consultar: ').lower().strip()
 
-print('Procurando...')
-time.sleep(6)
+def buscar_pokemon():
+    
+    name = input('Digite o nome do pokemon que deseja consultar: ').lower().strip()
 
-print('Aguarde só mais um pouco')
-time.sleep(5)
+    print('Procurando...')
+    time.sleep(6)
 
-pokemon_info = get_pokemon_info(name)
+    print('Aguarde só mais um pouco')
+    time.sleep(5)
 
-if pokemon_info:
-    print('Finalizando pesquisas')
-    time.sleep(1)
-    
-    print(f"Nome: {pokemon_info['name'].capitalize()}")
-    print(f"Id: {pokemon_info['id']}")
-    print(f"Altura: {pokemon_info['height']}")
-    print(f"Peso: {pokemon_info['weight']}")
-    print("Habilidades: ")
-    
-    for habilidade in pokemon_info['abilities']:
-        print(habilidade['ability']['name'].capitalize())
-        
-    print("Tipo: ")
-    
-    for tipo in pokemon_info['types']:
-        print(tipo['type']['name'].capitalize())
-        
-else:
-    print('Pokémon não encontrado')
-    
+    pokemon_info = get_pokemon_info(name)
+
+    if pokemon_info:
+        print('Finalizando pesquisas')
+        time.sleep(1)
+                
+        print(f"Nome: {pokemon_info['name'].capitalize()}")
+        print(f"Id: {pokemon_info['id']}")
+        print(f"Altura: {pokemon_info['height']}")
+        print(f"Peso: {pokemon_info['weight']}")
+        print("Habilidades: ")
+                
+        for habilidade in pokemon_info['abilities']:
+            print(habilidade['ability']['name'].capitalize())
+                    
+        print("Tipo: ")
+                
+        for tipo in pokemon_info['types']:
+            print(tipo['type']['name'].capitalize())
+                    
+    else:
+        print('Pokémon não encontrado')
+
+def historico():
+    print('Erro: Trabalhando nisso')
+
+
+menu()
