12d863e0d1df9359630cbcc50e3d5fc5128ee9f5
Pedro <ricardoepedroclash@gmail.com>
2026-06-26 13:36:45 -0300

    fix: converte porta do banco para inteiro em database.py

--- src/database.py
+++ src/database.py
@@ -8,16 +8,27 @@ load_dotenv()
 
 # Cria uma nova conexão com o banco MySQL
 def conectar():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=os.getenv("DB_PORT")
-    )
+    try:
+        conexao = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT")), # <-- Adicione int() aqui
+            use_pure=True
+        )
+
+        print("Conectado com sucesso")
+        return conexao
+
+    except mysql.connector.Error as erro:
+        print("Erro ao conectar:")
+        print(erro)
+        raise
 
 
 def salvar_historico(pokemon):
+    print('Entrou na função')
     cursor = None
     conexao = None
     
@@ -29,9 +40,7 @@ def salvar_historico(pokemon):
             "INSERT INTO historico_pesquisas (pokemon) VALUES (%s)",
             (pokemon,)
         )
-
         conexao.commit()
-
     except mysql.connector.Error as erro:
         print(erro)
 
